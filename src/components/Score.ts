b52f4af00fa9704a66e9418a8d8597fb05a1b74b
Alberto Rodriguez <arodu.dev@gmail.com>
2020-11-21 15:40:40 -0400

    deploy

--- src/components/Score.ts
+++ src/components/Score.ts
@@ -10,10 +10,12 @@ export default class Score {
 
   private counterLines: any;
   private counterLevel: any;
+  private counterPoints: any;
 
   constructor() {
     this.counterLines = document.querySelector("#counter-lines");
     this.counterLevel = document.querySelector("#counter-level");
+    this.counterPoints = document.querySelector("#counter-points");
   }
 
   public reset = () => {
@@ -37,9 +39,26 @@ export default class Score {
     this.counterLevel.innerHTML = this.level + 1;
   };
 
+  public checkPoints = (lines: number) => {
+    if (lines == 1) {
+      this.points += (this.level + 1) * 40;
+    }
+    if (lines == 2) {
+      this.points += (this.level + 1) * 100;
+    }
+    if (lines == 3) {
+      this.points += (this.level + 1) * 300;
+    }
+    if (lines == 4) {
+      this.points += (this.level + 1) * 1200;
+    }
+    this.counterPoints.innerHTML = this.points;
+  };
+
   public setLines = (successLines: number) => {
     this.lines += successLines;
     this.chechLevel();
+    this.checkPoints(successLines);
     this.counterLines.innerHTML = this.lines;
   };
 }
